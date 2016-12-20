61c4a1f9da04f1b9d022bb8950eaf8f6df5f87ff
Lili Feng <lili.feng@colorado.edu>
2016-12-20 11:27:38 -0700

    Dec 20th, 2016

--- run_fields002.py
+++ run_fields002.py
@@ -9,17 +9,22 @@ datadir='/lustre/janus_scratch/life9360/ses3d_working_dir_2016/OUTPUT'
 setupfile='/lustre/janus_scratch/life9360/ses3d_working_dir_2016/INPUT/setup'
 
 # # 
-#sfield=fields.ses3d_fields(datadir, setupfile, field_type = "velocity_snapshot")
-#mfname='vz'
-#sfield.convert_to_hdf5('/lustre/janus_scratch/life9360/ses3d_h5fields/EA_10sec_20sec_10.h5', mfname, depth=0, diter=10)
+sfield=fields.ses3d_fields(datadir, setupfile, field_type = "velocity_snapshot")
+mfname='vz'
+# sfield.convert_to_hdf5('/lustre/janus_scratch/life9360/ses3d_h5fields/EA_10sec_20sec_10.h5', mfname, depth=0, diter=10)
+# sfield.convert_to_hdf5('../example_for_Kuangdai/snapshot_for_kuangdai.h5', mfname, depth=0, iter0=14000, iterf=14000, diter=10)
 # 
-# dset=h5fields.h5fields('/lustre/janus_scratch/life9360/ses3d_h5fields/EA_10sec_20sec_10.h5')
+dset=h5fields.h5fields('/lustre/janus_scratch/life9360/ses3d_h5fields/EA_10sec_20sec_10.h5')
 # dset.zero_padding( outfname='/lustre/janus_scratch/life9360/ses3d_h5fields/EA_10sec_20sec_10_padding.h5', component='vz', minV=2.0, \
 #                 evlo=129.0, evla=41.306, dt=0.05)
 # dset.zero_padding( outfname='/lustre/janus_scratch/life9360/ses3d_h5fields/EA_10sec_20sec_10_padding_tt.h5', component='vz', minV=2.0, \
 #                 evlo=129.0, evla=41.306, dt=0.05, iter0=0, iterf=30000, diter=5000)
-dset2=h5fields.h5fields('/lustre/janus_scratch/life9360/ses3d_h5fields/EA_10sec_20sec_10_padding.h5')
-dset2.convert_to_vts(outdir='/lustre/janus_scratch/life9360/ses3d_vts/EA_vts_10', component='vz')
+
+dset.zero_padding( outfname='../example_for_Kuangdai/snapshot_for_kuangdai.h5', component='vz', minV=2.0, \
+                evlo=129.0, evla=41.306, dt=0.05, iter0=14000, iterf=14000, diter=5000)
+
+# dset2=h5fields.h5fields('/lustre/janus_scratch/life9360/ses3d_h5fields/EA_10sec_20sec_10_padding.h5')
+# dset2.convert_to_vts(outdir='/lustre/janus_scratch/life9360/ses3d_vts/EA_vts_10', component='vz')
 # dset2.plot_depth_slice('vz', iteration=25000, vmin=-5e-7, vmax=5e-7)
 # dset.plot_snapshots(component='vz', vmin=-3e-6, vmax=3e-6, outdir='/lustre/janus_scratch/life9360/h5test_snapshot' )
 #dset.plot_snapshots_mp(component='vz', vmin=-5e-7, vmax=5e-7, outdir='/lustre/janus_scratch/life9360/h5test_snapshot_US')
